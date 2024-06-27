550582c13537ee9cdfb1c695dd10d857a46213cd
Mohammad Ehson <30387730+ehsonmiraz@users.noreply.github.com>
2024-06-27 16:06:36 +0530

    Update settings.py

--- camp_aid/settings.py
+++ camp_aid/settings.py
@@ -81,10 +81,23 @@ WSGI_APPLICATION = 'camp_aid.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': ‘camp_aid’,
+
+        'USER': 'ehson',
+
+        'PASSWORD': '****',
+
+        'HOST': '****',
+
+        'PORT': '****',
+
     }
+
 }
 
 
